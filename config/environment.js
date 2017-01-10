81cc5a3768b1ad537bbcaa5b139368147daed4c3
James Keane <jpkeane@me.com>
2017-01-10 23:13:58 +0000

    Added HTTPS staging host

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.host = 'http://jk-tut-login-be-staging.herokuapp.com';
+    ENV.host = 'https://jk-tut-login-be-staging.herokuapp.com';
   }
 
   return ENV;
